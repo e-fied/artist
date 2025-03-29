2dfc7ba376c8d1c1f80e19c6ba4fd7227cfc2e33
e-fied <efied@live.com>
2025-03-29 03:45:58 -0700

    mobiel style

--- add_artist_type.sql
+++
@@ -1,7 +0,0 @@
--- Check if the column exists first
-PRAGMA table_info(artist);
-
--- Add the column if it doesn't exist
--- Note: SQLite doesn't support IF NOT EXISTS for ALTER TABLE ADD COLUMN
--- You'll need to run this command manually if the column doesn't exist
-ALTER TABLE artist ADD COLUMN artist_type VARCHAR(20) DEFAULT 'music'; 
